9f76f5f76f23b7a5b80b739720d7904248645e7a
maxufeng <unf-apqb330d4@dingtalk.com>
2024-04-30 14:27:05 +0800

    feat: 箭头函数

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 /**
  *  babel watch 配置更新後無觸發重建，僅監聽目錄發生變更
  */
+const runObject = {
+  add(a, b, ...any) {
+    if (any) {
+      return a + b + this.add(...any);
+    } else {
+      return a + b;
+    }
+  },
+  Add: (a, b, ...any) => {
+    if (any) {
+      return a + b + this.add(...any);
+    } else {
+      a + b;
+    }
+  }
+}
